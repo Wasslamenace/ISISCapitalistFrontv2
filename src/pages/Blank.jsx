19de4d341e2734aa9a340e6395945b0690b0fa99
Wassim_Aityoucef <58562600+Wasslamenace@users.noreply.github.com>
2022-02-23 12:30:01 +0100

    btn prix plus swagg

--- src/pages/Blank.jsx
+++ src/pages/Blank.jsx
@@ -7,16 +7,31 @@ const Blank = () => {
     <div class="username"> ID du joueur </div>
     </div>
     <div class="main">
+    
     <div class="product">
     <div> <span class="nameprod">JOUEUR DE LIGUE 1</span></div>
     <div><button class="prix"><h1>3$</h1></button></div>
     <div class="description"> Joueur de base du championnat de Ligue 1</div>
     </div>
+    
     <div class="product">
     <div> <span class="nameprod">JOUEUR DE LIGA</span></div>
     <div><button class="prix"><h1>6$</h1></button></div>
     <div class="description"> Joueur de base du championnat de Liga</div>
     </div>
+    
+    <div class="product">
+    <div> <span class="nameprod">JOUEUR D'EUROPA LEAGUE</span></div>
+    <div><button class="prix2"><h1>3500$</h1></button></div>
+    <div class="description"> Joueur d'un club de haut niveau Européen</div>
+    </div>
+
+    <div class="product">
+    <div> <span class="nameprod">JOUEUR DE CHAMPIONS LEAGUE </span></div>
+    <div><button class="prix2"><h1>10000$</h1></button></div>
+    <div class="description"> Joueur d'un club de l'élite européenne</div>
+    </div>
+
     </div>
    </div>;
 };
