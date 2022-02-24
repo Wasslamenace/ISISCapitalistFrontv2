5e4ffaf4ab0b0af8b6d926bf7c87c39acf74731a
Wassim_Aityoucef <58562600+Wasslamenace@users.noreply.github.com>
2022-02-24 09:44:29 +0100

    dev page managers + nvx ajouts

--- src/pages/Blank.jsx
+++ src/pages/Blank.jsx
@@ -6,31 +6,48 @@ const Blank = () => {
     <div><button class="button"><span>x 1</span></button></div>
     <div class="username"> ID du joueur </div>
     </div>
+
+    <div class="titre"><h1>Unlocks</h1></div>
+        <div class="desc"> Recrute tes joueurs afin de monter ton club !</div>
+
     <div class="main">
     
     <div class="product">
-    <div> <span class="nameprod">JOUEUR DE LIGUE 1</span></div>
+    <div> <span class="nameprod">JOUEUR AMATEUR</span></div>
     <div><button class="prix"><h1>3$</h1></button></div>
-    <div class="description"> Joueur de base du championnat de Ligue 1</div>
+    <div class="description"> Boulanger et footballer à ses heures perdues, joueur du dimanche</div>
     </div>
     
     <div class="product">
-    <div> <span class="nameprod">JOUEUR DE LIGA</span></div>
+    <div> <span class="nameprod">JOUEUR PROFESSIONNEL</span></div>
     <div><button class="prix"><h1>6$</h1></button></div>
-    <div class="description"> Joueur de base du championnat de Liga</div>
+    <div class="description"> Joueur pro, joue dans un bon club de niveau national</div>
     </div>
     
     <div class="product">
-    <div> <span class="nameprod">JOUEUR D'EUROPA LEAGUE</span></div>
+    <div> <span class="nameprod">JOUEUR D'UEFA EL</span></div>
     <div><button class="prix2"><h1>3500$</h1></button></div>
-    <div class="description"> Joueur d'un club de haut niveau Européen</div>
+    <div class="description"> Joueur d'Europa League, joue à un bon niveau Européen</div>
     </div>
 
     <div class="product">
-    <div> <span class="nameprod">JOUEUR DE CHAMPIONS LEAGUE </span></div>
+    <div> <span class="nameprod">JOUEUR D'UEFA CL </span></div>
     <div><button class="prix2"><h1>10000$</h1></button></div>
-    <div class="description"> Joueur d'un club de l'élite européenne</div>
+    <div class="description"> Joueur de Champions League,fait partie de l'élite européenne</div>
     </div>
+
+    <div class="product">
+    <div> <span class="nameprod">JOUEUR STAR </span></div>
+    <div><button class="prix2"><h1>200000$</h1></button></div>
+    <div class="description"> Joueur d'une classe et d'une renommée rarement atteinte, une légende à en devenir</div>
+    </div>
+
+    <div class="product">
+    <div> <span class="nameprod">JOUEUR DE LEGENDE </span></div>
+    <div><button class="prix2"><h1>500000$</h1></button></div>
+    <div class="description"> Légende du football, joueur ayant marqué l'histoire du sport</div>
+    </div>
+
 
     </div>
    </div>;
--- src/pages/Managers.jsx
+++ src/pages/Managers.jsx
@@ -1,5 +1,31 @@
+import "./style/managers.scss"
+
 const Managers = () => {
-    return <div>This is Managers page</div>;
+    return <div> 
+        <div class="titre"><h1>Managers</h1></div>
+        <div class="desc"> Recrute des managers afin d'automatiser tes achats !</div>
+
+    <div class="manager">
+    <div> <span class="namemanager"> Lucas Abitbol </span></div>
+    <div><button class="prix3"><h1>50000$</h1></button></div>
+    <div class="description">Mr Abibtol est un spécialiste et un amoureux du football amateur, il recrutera automatiquement vos joueurs amateurs</div>
+    </div> 
+
+    <div class="manager">
+    <div> <span class="namemanager"> Wassim Ait Youcef </span></div>
+    <div><button class="prix3"><h1>120000$</h1></button></div>
+    <div class="description">Mr Ait Youcef est un tacticien qui sait trouver les pépites nationales, il recrutera automatiquement vos joueurs professionels</div>
+    </div> 
+
+    <div class="manager">
+    <div> <span class="namemanager"> José Mourinho </span></div>
+    <div><button class="prix3"><h1>340000$</h1></button></div>
+    <div class="description">Mr Mourinho est un grand entraîneur à fort caractère de classe européenne, il recrutera automatiquement vos joueurs d'Europa League</div>
+    </div> 
+    
+    
+    </div>;
+    
 };
 
 export default Managers;
